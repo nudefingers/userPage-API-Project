401a1efac7e45cfb8cbceec54241cca3c17d4ee8
nudefingers <nude.fingers@gmail.com>
2023-07-20 18:02:32 +0300

    done feature

--- DataManager.js
+++ DataManager.js
@@ -4,10 +4,13 @@ const USER_URL = `https://randomuser.me/api/?results=`
 const QUOTE_URL = `https://api.kanye.rest/`
 const BACON_URL = `https://baconipsum.com/api/?type=all-meat&paras=1`
 const POKE_URL = `https://pokeapi.co/api/v2/pokemon/`
+const GIPHY_URL = `http://api.giphy.com/v1/gifs/search?q=`
+const GIPHY_KEY = `&api_key=RI1CIXCll0k2LGXsHrH0DRkv859fx6vX&limit=5`
 
 
 class DataManager {
     static _users = []
+    current = ``
 
     constructor() {
         return Promise.all([
@@ -18,46 +21,55 @@ class DataManager {
         ]).then(() => this)
     }
 
-    get user()    { return this._user    }
-    get quote()   { return this._quote   }
+    get user() { return this._user }
+    get quote() { return this._quote }
+    get giphy() { return this._giphy }
     get friends() { return this._friends }
     get pokemon() { return this._pokemon }
     get aboutMe() { return this._aboutMe }
 
     generateUsers() {
         return this.fetchData(`${USER_URL}${USERS_AMOUNT}`)
-        .then(users => {
-            const [firstUser, ...remainingUsers] = users.results
-            this._user = {
-                photo: firstUser.picture.large,
-                name: `${firstUser.name.first} ${firstUser.name.last}`,
-                city: firstUser.location.city,
-                state: firstUser.location.state
-            }
-            this._friends = remainingUsers.map(u => u.name).map(n => `${n.first} ${n.last}`)
-        })
+            .then(users => {
+                const [firstUser, ...remainingUsers] = users.results
+                this._user = {
+                    photo: firstUser.picture.large,
+                    name: `${firstUser.name.first} ${firstUser.name.last}`,
+                    city: firstUser.location.city,
+                    state: firstUser.location.state
+                }
+                this._friends = remainingUsers.map(u => u.name).map(n => `${n.first} ${n.last}`)
+            })
     }
 
     requestQuote() {
         return this.fetchData(QUOTE_URL)
-        .then(data => {
-            this._quote = data.quote
-        })
+            .then(data => {
+                this._quote = data.quote
+            })
     }
 
     requestPokemon() {
         let randomId = Math.floor(Math.random() * TOTAL_POKE_NUM) + 1
         return this.fetchData(`${POKE_URL}${randomId}`)
-        .then(data => {
-            this._pokemon = { name: data.name, image: data.sprites.front_default }
-        })
+            .then(data => {
+                this.generateGiphy(data.name)
+                this._pokemon = { name: data.name, image: data.sprites.front_default, type: data.types[0].type.name }
+            })
     }
 
     requestAboutMeatText() {
         return this.fetchData(BACON_URL)
-        .then(data => {
-            this._aboutMe = data[0]
-        })
+            .then(data => {
+                this._aboutMe = data[0]
+            })
+    }
+
+    generateGiphy(pokeName) {
+        return this.fetchData(`${GIPHY_URL}${pokeName}${GIPHY_KEY}`)
+            .then(data => {
+                this._giphy = data.data[0].images.downsized_large.url
+            })
     }
 
     fetchData(url) {
--- Renderer.js
+++ Renderer.js
@@ -16,6 +16,8 @@ class Renderer {
 
         $('.container').html()
         $(`.container`).append(newHTML, newHTMLF)
+
+        this.changeBackgroundColor(user.pokemon.type)
     }
 
     static viewSavedUsers(users) {
@@ -32,7 +34,7 @@ class Renderer {
         })
 
         $.each(users, (_, user) => {
-            let savedUserDiv = $("<div>", { class: "saved-user", text: `${user}` })
+            let savedUserDiv = $(`<div>`, { class: "saved-user", text: `${user}` })
             popup.append(savedUserDiv)
         })
 
@@ -41,6 +43,18 @@ class Renderer {
 
     static clean() {
         $(`.container`).children().not(`.buttons`).remove()
+        $(`body`).css(`background-color`, `#ecf0f1`)
         $(`#popup`).hide()
+    }
+
+    static changeBackgroundColor(type) {
+        const colors = {water: `aqua`, dragon: `orange`, ice: `blue`, fire: `yellow`, fairy: `violet`, grass: `green`}
+        let color = colors[type]
+
+        if(!color) { 
+            color = `pink`
+        }
+        
+        $(`body`).css(`background-color`, color)
     }
 }
